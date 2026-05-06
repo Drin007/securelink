567998f38614dfdc8412c80f3e10197446a9b889
Drin007 <137424130+Drin007@users.noreply.github.com>
2026-05-06 09:36:28 +0530

    fix: simplify CORS for deployment

--- server/index.js
+++ server/index.js
@@ -17,12 +17,9 @@ const app = express();
 
 
 app.use(cors({
-  origin: ['http://localhost:5173'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: true,
   credentials: true
 }));
-
 
 app.use(express.json());
 
